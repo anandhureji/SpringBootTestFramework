14caa46a9c57095fa7213cd4af7adaa1c0eef37f
anandhu <anandhureji33@gmail.com>
2023-04-03 15:54:30 +0530

    mockito test cases for exceptions

--- src/test/java/com/springboottesting1/testspringboot1/service/EmployeeServiceTests.java
+++ src/test/java/com/springboottesting1/testspringboot1/service/EmployeeServiceTests.java
@@ -1,12 +1,18 @@
 package com.springboottesting1.testspringboot1.service;
 
 import com.springboottesting1.testspringboot1.Repository.EmployeeRepository;
+import com.springboottesting1.testspringboot1.exception.ResourceNotFoundException;
 import com.springboottesting1.testspringboot1.modal.Employee;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -23,8 +29,17 @@ public class EmployeeServiceTests {
     @InjectMocks
     private EmployeeServiceImpl employeeService;
 
+    private Employee employee;
+
     @BeforeEach
     public void setup(){
+
+        employee = Employee.builder()
+                .id(1L)
+                .firstName("Anandhu")
+                .lastName("Reji")
+                .email("anandhureji33@gmail.com")
+                .build();
 
         //employeeRepository= Mockito.mock(EmployeeRepository.class);
         //employeeService= new EmployeeServiceImpl(employeeRepository);
@@ -32,21 +47,39 @@ public class EmployeeServiceTests {
 
     @Test
     public void givenEmployeeObject_whenSaveEmployee_thenEmployeeObject(){
-        Employee employee = Employee.builder()
-                .id(1L)
-                .firstName("Anandhu")
-                .lastName("Reji")
-                .email("anandhureji33@gmail.com")
-                .build();
 
-        BDDMockito.given(employeeRepository.findByEmail(employee.getEmail()))
+
+        given(employeeRepository.findByEmail(employee.getEmail()))
                 .willReturn(Optional.empty());
-        BDDMockito.given(employeeRepository.save(employee))
+        given(employeeRepository.save(employee))
                 .willReturn(employee);
 
         Employee savedEmployee = employeeService.saveEmployee(employee);
 
         Assertions.assertThat(savedEmployee).isNotNull();
+
+
+
+    }
+
+    @Test
+    public void givenExistingEmail_whenSaveEmployee_thenThrowsException(){
+
+
+        given(employeeRepository.findByEmail(employee.getEmail()))
+                .willReturn(Optional.of(employee));
+
+
+        org.junit.jupiter.api.Assertions.assertThrows(ResourceNotFoundException.class,()->{
+            employeeService.saveEmployee(employee);
+
+        });
+
+        verify(employeeRepository,never()).save(any(Employee.class));
+
+
+
+
 
 
 
